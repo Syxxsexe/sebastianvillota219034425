31d0e2725cedeee18e026e5a2580fea5daad7868
Syxxsexe <sebas6310l@gmail.com>
2024-09-27 20:33:38 -0500

    Feat: al dia

---
+++ CGpracticas/my-proyect-angular-2/src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { ContadorComponent } from './contador/contador.component';
+
+export const routes: Routes = [
+
+    {
+    path:'contador',
+    component:ContadorComponent
+
+    }
+
+
+];
