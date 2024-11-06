29ab83428f5785ce56336300b7c13ae95e1e2197
Syxxsexe <sebas6310l@gmail.com>
2024-11-05 23:26:10 -0500

    feat: so close

--- PracticaFormulario/src/app/app.component.ts
+++ PracticaFormulario/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { DatosGerenteComponent } from './components/datos-gerente/datos-gerente.component';
 import { DetallesLicenciaComponent } from './components/detalles-licencia/detalles-licencia.component';
 import { NombreYContactoComponent } from './components/nombre-ycontacto/nombre-ycontacto.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,DatosGerenteComponent,DetallesLicenciaComponent,NombreYContactoComponent, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- PracticaFormulario/src/app/app.routes.ts
+++ PracticaFormulario/src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
-
+import { DatosGerenteComponent } from './components/datos-gerente/datos-gerente.component';
+import { DetallesLicenciaComponent } from './components/detalles-licencia/detalles-licencia.component';
+import { NombreYContactoComponent } from './components/nombre-ycontacto/nombre-ycontacto.component';
 export const routes: Routes = [];
